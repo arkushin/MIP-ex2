ba04f65da04c865425150ac413c2f4a0cc9b35b7
arkushin <daniel.arkushin@mail.huji.ac.il>
2018-11-22 17:01:13 +0200

    first part complete

--- ex2.py
+++ ex2.py
@@ -1,9 +1,9 @@
 import nibabel as nib
 import numpy as np
-import ntpath
 from skimage import measure, morphology
 import matplotlib.pyplot as plt
-import copy
+from scipy.signal import argrelextrema
+
 
 IMAX = 1300
 
@@ -15,10 +15,9 @@ def SegmentationByTH(path, Imin, Imax):
     :param Imin: The lower threshold for the segmentation
     :param Imax: The upper threshold for the segmentation
     """
+    file_name = path.split('.')[0]
     img = nib.load(path)
     img_data = img.get_data()
-    # file_name = ((ntpath.basename(path)).split('.'))[0]
-    file_name = path.split('.')[0]
 
     # segment the image using Imin as the lower threshold and Imax as the upper threshold
     img_data[img_data < Imin] = 0
@@ -31,130 +30,67 @@ def SegmentationByTH(path, Imin, Imax):
     return img_data
 
 
-def test(path):
-    img = nib.load(path)
-    img_data = img.get_data()
-    img_data[::] = 0
-    nib.save(img, 'test_seg.nii.gz')
-
-
 def SkeletonTHFinder(path):
+    """
+    A fucnction that iterates over different imin values, chooses the best one of them and then creates a skeleton
+    segmentation of the given file using the best imin and morphological operations
+    :param path: The path to the file that should be segmented
+    :return: The function saves the segmentation of the skeleton and returns the best imin according to which the
+    function produced the skeleton segmentation
+    """
     file_name = path.split('.')[0]
-    # connected_components_list = []
-    # imin_list = []
-    # for imin in range(150, 500, 14):
-    #     imin_list.append(imin)
-    #     print(imin)
-    #     segmentation = SegmentationByTH(path, imin, IMAX)
-    #     labels, connected_components_num = measure.label(segmentation, return_num=True)
-    #     connected_components_list.append(connected_components_num)
+
+    # segment the file with different Imin values between 150 and 500
+    connected_components_list = []
+    imin_list = []
+    for imin in range(150, 500, 14):
+        imin_list.append(imin)
+        print(imin)
+        segmentation = SegmentationByTH(path, imin, IMAX)
+        labels, connected_components_num = measure.label(segmentation, return_num=True)
+        connected_components_list.append(connected_components_num)
 
     # create and save the graph of Imin vs. number of connectivity components
-    # plt.figure()
-    # plt.plot(imin_list, connected_components_list)
-    # plt.title(file_name + ': Number of connectivity components as function of Imin')
-    # plt.savefig(file_name + '_Graph.jpg')
-    #
-    # best_imin = imin_list[connected_components_list.index(min(connected_components_list))]
-    # print(best_imin)
+    plt.figure()
+    plt.plot(imin_list, connected_components_list)
+    plt.title(file_name + ': Number of connectivity components as function of Imin')
+    plt.savefig(file_name + '_Graph.jpg')
 
-    best_imin = 248
+    # find best Imin:
+    minimas_list = argrelextrema(np.array(connected_components_list), np.less)[0]
+    best_imin = imin_list[minimas_list[0]]
+    if len(minimas_list) > 1:
+        second_minima = imin_list[minimas_list[1]]
+        if second_minima < best_imin and second_minima < 300:
+            best_imin = second_minima
+
     # load segmentation file of the best Imin
     best_segmentation = nib.load(file_name + '_seg_' + str(best_imin) + '_' + str(IMAX) + '.nii.gz')
-
-    # best_segmentation = do_morphological_operations(best_segmentation)
     im_data = best_segmentation.get_data()
-    print('start labels: ', np.unique(im_data))
-
-    # img_data = do_morphological_operations(img_data)
-
-    selem = morphology.cube(5)
-
-    # img_data = segmented_image.get_data()
-    # img_data = data
+    selem = morphology.cube(7)
 
     # perform image closing in order to fill holes and connect related areas
-    morphology_flags = morphology.binary_closing(im_data, selem)
+    closing_flags = morphology.binary_closing(im_data, selem)
     im_data[::] = 0
-    im_data[morphology_flags] = 1
+    im_data[closing_flags] = 1
 
-    print('after closing: ', np.unique(im_data))
-
-    im_data, new_connected_components_num = measure.label(im_data, return_num=True)
-    print('after labeling: ', new_connected_components_num)
-    print('labels after labeling: ', np.unique(im_data))
-
-    props = measure.regionprops(im_data)
+    # label the different connectivity components and remove all except the biggest component
+    im_data[::], new_connected_components_num = measure.label(im_data, return_num=True)
+    props = measure.regionprops(im_data.astype(np.uint16))
     max_area = props[0].area
-    print('searching for max area...')
     for i in range(1, new_connected_components_num):
         if props[i].area > max_area:
             max_area = props[i].area
-    print('max area: ', max_area)
-    im_data = morphology.remove_small_objects(im_data, 2 * max_area, in_place=True)
-    print('after objects removal: ', np.unique(im_data))
+    im_data[::] = morphology.remove_small_objects(im_data.astype(np.uint16), max_area)
+    im_data[im_data != 0] = 1
+    im_data[::] = morphology.remove_small_holes(im_data.astype(np.uint16), area_threshold=max_area)
 
-
-    new_im_data = best_segmentation.get_data()
-    print('new im data labels: ', np.unique(new_im_data))
-    print(np.array_equal(new_im_data, im_data))
-
-    # img_data[::] = 0
-    # print('setting img_data to 0')
-    # img_data[small_objects_flags == 1] = 1
-    # print('done with setting values')
-    # labels, new_connected_components_num = measure.label(img_data, return_num=True)
-    # print('after removal: ', new_connected_components_num)
-
-    # img_data[img_data != 0] = 1
-
-
+    # save the resulting skeleton segmentation and return the best Imin
     nib.save(best_segmentation, file_name + '_SkeletonSegmentation.nii.gz')
-
-
-#
-# def do_morphological_operations(data):
-#     """
-#     A function that gets an labeled image, and using morphological operations turns the image into one connectivity
-#     component
-#     :param segmented_image: The image that should be processed recieved as a nii.gz file
-#     :return: The image with only one connectivity component
-#     """
-#     selem = morphology.cube(5)
-#
-#     # img_data = segmented_image.get_data()
-#     # img_data = data
-#
-#     # perform image closing in order to fill holes and connect related areas
-#     morphology_flags = morphology.binary_closing(img_data, selem)
-#     img_data[::] = 0
-#     img_data[morphology_flags] = 1
-#
-#     img_data, new_connected_components_num = measure.label(img_data, return_num=True)
-#     print('after closing: ', new_connected_components_num)
-#     props = measure.regionprops(img_data)
-#     max_area = props[0].area
-#     print('searching for max area...')
-#     for i in range(1, new_connected_components_num):
-#         if props[i].area > max_area:
-#             max_area = props[i].area
-#     print('max area: ', max_area)
-#     small_objects_flags = morphology.remove_small_objects(img_data, 2*max_area)
-#     print('after objects removal')
-#     img_data[::] = 0
-#     print('setting img_data to 0')
-#     img_data[small_objects_flags == 1] = 1
-#     print('done with setting values')
-#     labels, new_connected_components_num = measure.label(img_data, return_num=True)
-#     print('after removal: ', new_connected_components_num)
-#     return img_data
-#
-
+    return best_imin
 
 
 if __name__ == '__main__':
-    path = "Case1_CT.nii.gz"
-    SkeletonTHFinder(path)
-    # segmentation = SegmentationByTH(path, 234, IMAX)
-    # labels, num = measure.label(segmentation, return_num=True)
-    # print(num)
+    path = "Case2_CT.nii.gz"
+    print('best imin: ', SkeletonTHFinder(path))
+
